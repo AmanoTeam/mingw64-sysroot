24c24e99bbbe0e4aa199e5bbc3f836920fae2e8a
Kartatz <105828205+Kartatz@users.noreply.github.com>
2025-12-12 01:58:54 -0300

    Initial commit

--- main.sh
+++ main.sh
@@ -40,6 +40,21 @@ tar \
 	--extract \
 	--file="${sysroot_tarball}"
 
+curl \
+	--url 'https://archlinux.org/packages/extra/any/mingw-w64-winpthreads/download/' \
+	--retry '30' \
+	--retry-delay '0' \
+	--retry-all-errors \
+	--retry-max-time '0' \
+	--location \
+	--silent \
+	--output "${sysroot_tarball}"
+
+tar \
+	--directory="$(dirname "${sysroot_directory}")" \
+	--extract \
+	--file="${sysroot_tarball}"
+
 for triplet in "${targets[@]}"; do
 	declare tarball_filename="/tmp/${triplet}.tar.xz"
 	tar --directory="${sysroot_directory}" --create --file=- "${triplet}" | xz --threads='0' --compress -9 > "${tarball_filename}"
